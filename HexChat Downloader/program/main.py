95638366c06ea4313938608b90c5863fbcbc399c
Hermann Krumrey <uodxh@student.kit.edu>
2015-05-06 20:01:22 +0200

    Added main module.

---
+++ HexChat Downloader/program/main.py
@@ -0,0 +1,10 @@
+"""
+main
+The main part of the program that combines all the different submodules to form a working program.
+
+Created on May 6, 2015
+Modified on May 6, 2015
+
+@author Hermann Krumrey
+@version 0.1
+"""
