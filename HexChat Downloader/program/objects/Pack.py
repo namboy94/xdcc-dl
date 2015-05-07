e5a959c749e21610bb84380ba77c4ac9e359c203
Hermann Krumrey <uodxh@student.kit.edu>
2015-05-07 23:10:31 +0200

    Created barebones structure for Pack class.

---
+++ HexChat Downloader/program/objects/Pack.py
@@ -0,0 +1,26 @@
+"""
+Pack
+Class that models a pack, complete with server and channel info
+
+Created on May 6, 2015
+Modified on May 6, 2015
+
+@author Hermann Krumrey
+@version 0.1
+"""
+
+#imports
+
+"""
+Pack
+The class that models a pack
+"""
+class Pack(object):
+    
+    """
+    Constructor
+    constructs a new Pack object
+    @param
+    """
+    def __init__(self,packString):
+        self.placeholder
