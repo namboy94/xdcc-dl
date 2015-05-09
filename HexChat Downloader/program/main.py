8a729730d2ca0a207538f6c1377f9697fe798d7a
Hermann Krumrey <uodxh@student.kit.edu>
2015-05-09 13:38:23 +0200

    Implemented OS check.

--- HexChat Downloader/program/main.py
+++ HexChat Downloader/program/main.py
@@ -8,7 +8,32 @@ Modified on May 7, 2015
 @author Hermann Krumrey
 @version 0.1
 """
+
+#imports
+import sys
+import platform
 from program.parsers.serverParser import serverParse
+
+#OS check and setup
+if platform.system() == "Linux":
+    #fix pythonpath
+    splitPath = sys.argv[0].split("/")
+    lengthToCut = len(splitPath[len(splitPath) - 1]) + len(splitPath[len(splitPath) - 2]) + 2
+    upperDirectory = sys.argv[0][:-lengthToCut]
+    sys.path.append(upperDirectory)
+    
+    #set serverFile
+    serverFile = upperDirectory + "/program/data/servers.config"
+
+elif platform.system() == "Windows":
+    print "Sorry, Windows is not supported yet"
+    sys.exit(1)
+else:
+    print "Sorry, this operating system is not supported"
+    sys.exit(1)
+
+botList = []
+serverParse(serverFile,botList)
 
 #TODOS
 
@@ -17,7 +42,3 @@ from program.parsers.serverParser import serverParse
 # ScriptCreator
 # ScriptMover
 # HexStarter
-
-serverFile = ""
-botList = []
-serverParse(serverFile,botList)
