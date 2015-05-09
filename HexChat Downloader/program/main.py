6cbfbac418f9507b0c13a414e65f077aafe3c24a
Hermann Krumrey <uodxh@student.kit.edu>
2015-05-09 14:05:03 +0200

    Added initial ScriptCreator construct.

--- HexChat Downloader/program/main.py
+++ HexChat Downloader/program/main.py
@@ -3,7 +3,7 @@ main
 The main part of the program that combines all the different submodules to form a working program.
 
 Created on May 6, 2015
-Modified on May 7, 2015
+Modified on May 9, 2015
 
 @author Hermann Krumrey
 @version 0.1
---
+++ HexChat Downloader/program/utils/ScriptCreator.py
@@ -0,0 +1,30 @@
+"""
+ScriptCreator
+Class that creates a hexchat python script, moves it into the hexchat auto load directory,
+and finally executes hexchat
+
+Created on May 9, 2015
+Modified on May 9, 2015
+
+@author Hermann Krumrey
+@version 0.1
+"""
+
+#imports
+
+
+"""
+ScriptCreator
+the class that acts as bridge to Hexchat
+"""
+class ScriptCreator(object):
+    
+    """
+    Constructor
+    Initializes a new ScriptCreator object
+    @param packList - list of packs to be downloaded
+    @param botList - list of bots 
+    """
+    def __init__(self,packList,botList):
+        self.botList = botList
+        self.packList = packList
