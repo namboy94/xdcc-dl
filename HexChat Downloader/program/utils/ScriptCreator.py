bfb95eba82be9f4eaa9a51b1e8e7f540562a0982
Hermann Krumrey <uodxh@student.kit.edu>
2015-05-10 17:44:59 +0200

    Implemented botPackWriter and scriptInitializer

--- HexChat Downloader/program/utils/ScriptCreator.py
+++ HexChat Downloader/program/utils/ScriptCreator.py
@@ -26,7 +26,7 @@ class ScriptCreator(object):
     @param packList - list of packs to be downloaded
     @param botList - list of bots 
     """
-    def __init__(self,packList,botList):
+    def __init__(self,packList,botList,scriptFile):
         
         self.botList = botList
         for pack in packList:
@@ -37,7 +37,39 @@ class ScriptCreator(object):
                     added = True
             if not added:
                 print "Bot %s not found." % (pack.botName)
+        
+        self.scriptInitializer(scriptFile)
+        for bot in self.botList:
+            self.botPackWriter(bot)
+            
+        self.scriptFile.close()
                     
     """
-    packWriter
+    botPackWriter
     """
+    def botPackWriter(self,bot):
+        
+        for pack in bot.packs:
+            self.scriptFile.write("def join_" + pack.botName + pack.packNumber + "(word, word_eol, userdata):\n")
+            self.scriptFile.write("\t'hexchat.command(msg " + pack.botName + " xdcc send #" + pack.packnumber + "'\n")
+            self.scriptFile.write("\treturn hexchat.EAT_HEXCHAT\n")
+            self.scriptFile.write("hexchat.command('newserver irc://" + bot.serverName + "/" + bot.channelName + "')\n")
+            self.scriptFile.write("hexchat.hook_print(\"You Join\", join_" + pack.botName + pack.packNumber + ")\n")
+            self.scriptFile.write("hexchat.hook_print(\"DCC RECV Complete\", quitChannel\n\n")
+        
+        
+    """
+    scriptInitializer
+    """
+    def scriptInitializer(self,scriptFile):
+        
+        self.scriptFile = open(scriptFile, "a")
+        self.scriptFile.write("__module_name__ = \"xdcc_executer\"\n")
+        self.scriptFile.write("__module_version__ = \"0.1\"\n")
+        self.scriptFile.write("__module_description__ = \"Python XDCC Executer\"\n\n")
+        self.scriptFile.write("import hexchat\n\n")
+        self.scriptFile.write("def quitChannel(word, word_eol, userdata):\n")
+        self.scriptFile.write("\thexchat.command('quit')\n")
+        self.scriptFile.write("\t return hexchat.EAT-HEXCHAT\n\n")
+        
+        
