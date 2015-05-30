faf308619a326bf23839f3133577d3569ecb32fc
hermann <hermann@krumreyh.com>
2015-05-30 14:36:44 +0200

    Windows and CMD are literally the devil. Fixed small problem with starting hexchat from command line.

--- HexChat Downloader/program/main.py
+++ HexChat Downloader/program/main.py
@@ -42,8 +42,8 @@ elif platform.system() == "Windows":
     packFile = upperDirectory + "\\program\\data\\packs.txt"
     configFile = upperDirectory + "\\program\\data\\config.ini"
     scriptFile = upperDirectory + "\\program\\data\\temp\\script.py"
-    hexChatScriptDirectory = "C:\\Users\\" + getpass.getuser() + "\\AppData\\Roaming\\HexChat\\addons"
-    hexChatCommand = upperDirectory + "\\program\\resources\\HexChat 2.10.2 (x64) Portable\\HexChat\\hexchat.exe\\"
+    hexChatScriptDirectory = "C:\\Users\\" + getpass.getuser() + "\\AppData\\Roaming\\HexChat\\addons\\"
+    hexChatCommand = upperDirectory + "\\program\\resources\\HexChat 2.10.2 (x64) Portable\\HexChat\\hexchat.exe"
     
     if not os.path.isdir(hexChatScriptDirectory):
         os.system("\"" + hexChatCommand + "\"")
