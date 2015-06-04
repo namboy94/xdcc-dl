b8da896e25b6869a9a64f608f6738739550c5bfd
hermann <hermann@krumreyh.com>
2015-06-04 18:54:50 +0200

    Completed Object-oriented CLI Parser.

--- HexChat Downloader/program/parsers/CLI.py
+++ HexChat Downloader/program/parsers/CLI.py
@@ -47,11 +47,11 @@ class CLI(object):
             if self.userInput.startswith("/msg ") and " xdcc send #" in self.userInput: self.downloadSinglePack()
             if self.userInput.lower() == "edit packs": self.editPacks()
             if self.userInput.lower() == "edit servers": self.editServers()
-            if self.userInput.lower() == "print": self.placeHolder()
-            if self.userInput.lower() == "email log": self.placeHolder()
-            if self.userInput.lower() == "start": self.placeHolder()
-            if self.userInput.lower() == "switch": self.placeHolder()
-            if self.userInput.lower() == "quit": self.placeHolder()
+            if self.userInput.lower() == "print": self.printLog()
+            if self.userInput.lower() == "email log": self.emailLog()
+            if self.userInput.lower() == "start": self.startBatchDownload()
+            if self.userInput.lower() == "switch": self.switchToGUI()
+            if self.userInput.lower() == "quit": self.quit()
             
             if not self.validInput:
                 print "Input was not understood. Enter help for a list of valid commands"
@@ -122,92 +122,48 @@ class CLI(object):
         self.scriptWriter = ScriptCreator(packList, botList, self.scriptFile, self.scriptWriter.hexChatLocation, self.hexChatCommand)
         self.logger = Logger(self.scriptWriter,self.logger.emailSender,self.logger.emailReceiver,self.logger.emailServer,self.logger.emailPort,self.logger.emailPass)
 
-    def placeHolder(self):
-        print 1
+    """
+    Prints a log to the console
+    """
+    def printLog(self):
+        self.logger.printLogToConsole()
+        self.validInput = True
+        
+    """
+    Emails a log to the user's email adress
+    """
+    def emailLog(self):
+        self.logger.emailLog()
         self.validInput = True
 
-"""
-inputParser
-parses user input and acts accordingly
-@param packFile - the location of the packfile
-@param serverFile - the location of the serverfile
-@param scriptWriter - the scriptwriter loaded with the information of the pack- and server files.
-def inputParser(packFile, serverFile, scriptFile, scriptWriter, logger, config, hexChatCommand):
-    
-    running = True
-    
-    print "Welcome to the HexChat XDCC Downloader"
-    userInput = raw_input("What would you like to do?\n")
-    
-    while running:
-        
-        validInput = False
-        
-        #downloads all packs from the packlist file
-        if userInput == "start":
-            running = False
-            scriptWriter.scriptExecuter()
-            if config.emailSwitch: logger.emailLog()
-            break
-        
-        #quits the program
-        if userInput == "quit":
-            running = False
-            break
-        
-        #allows to edit packs with a text editor
-        if userInput == "edit packs":
-            if platform.system() == "Linux":
-                os.system(config.textEditor + " '" + packFile + "'")
-            if platform.system() == "Windows":
-                os.system("call \"" + config.textEditor + "\" \"" + packFile + "\"")
-            validInput = True
-            
-        #allows to edit the server config with editor
-        if userInput == "edit servers":
-            if platform.system() == "Linux":
-                os.system(config.textEditor + " '" + serverFile + "'")
-            if platform.system() == "Windows":
-                os.system("call \"" + config.textEditor + "\" \"" + serverFile + "\"")
-            validInput = True
-        
-        #prints the parsed packs to the console    
-        if userInput == "print":
-            logger.printLogToConsole()
-            validInput = True
-            
-        #sends an email to the user containing all packs to be downloaded
-        if userInput == "email log":
-            logger.emailLog()
-            validInput = True
-            
+    """
+    Starts a batch download of all packs in the packfile
+    """
+    def startBatchDownload(self):
+        self.scriptWriter.scriptExecuter()
+        if self.config.emailSwitch: self.logger.emailLog()
+        print "Download of all packs in packs.txt complete\n"
+        self.validInput = True
 
-            
-        #switches to GUI
-        if userInput == "switch":
-            lines = [line.rstrip('\n') for line in open(config.configFile)]
-            configFile = open(config.configFile, "w")
-            for line in lines:
-                if line.startswith("gui on = "):
-                    config.guiSwitch = True
-                    configFile.write("gui on = true\n")
-                else:
-                    configFile.write(line + "\n")
-            configFile.close()
-            break
-        
-        #Input not understood
-        if not validInput:
-            print ("Input was not understood")
-        
-        #refresh information
-        botList = []
-        packList = []    
-        serverParse(serverFile,botList)
-        packParse(packFile,packList)
-        scriptWriter = ScriptCreator(packList, botList, scriptFile, scriptWriter.hexChatLocation, hexChatCommand)
-        logger = Logger(scriptWriter,logger.emailSender,logger.emailReceiver,logger.emailServer,logger.emailPort,logger.emailPass)
-            
-        #next prompt
-        userInput = raw_input("What would you like to do?\n")
-        """
+    """
+    Switches to the GUI Interface
+    """
+    def switchToGUI(self):
+        lines = [line.rstrip('\n') for line in open(self.config.configFile)]
+        self.configFile = open(self.config.configFile, "w")
+        for line in lines:
+            if line.startswith("gui on = "):
+                self.config.guiSwitch = True
+                self.configFile.write("gui on = true\n")
+            else:
+                self.configFile.write(line + "\n")
+        self.configFile.close()
+        self.quit()
+        self.validInput = True
+
+    """
+    Quits the program
+    """
+    def quit(self):
+        self.running = False
+        self.validInput = True
