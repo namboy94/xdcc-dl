0ec8434b0023355c9c8830eaf4ca8787a5db16ac
hermann <hermann@krumreyh.com>
2015-06-04 09:46:18 +0200

    Implemented text box element of the GUI

--- HexChat Downloader/program/utils/GUI.py
+++ HexChat Downloader/program/utils/GUI.py
@@ -38,6 +38,9 @@ class DownloadGUI(object):
         self.config = config
         self.hexChatCommand = hexChatCommand
         
+        #variables used by gui elements
+        self.singlePackVar = ""
+        
     """
     starts the GUI
     """
@@ -49,6 +52,9 @@ class DownloadGUI(object):
         self.gui.title("HexChat Downloader GUI")
         self.gui.wm_resizable(False, False)
         
+        self.addButton("Single Pack Download", 10, 10, 200, 40, self.test)
+        self.addTextBox(self.singlePackVar, 400, 10, 200, 40, self.test2)
+        
         #Start GUI
         self.gui.mainloop()
         
@@ -56,15 +62,33 @@ class DownloadGUI(object):
     adds a button to the GUI
     @param text - the text to be displayed
     @param xPos - the x position in the window
-    @param yPos - the y position in the windows
+    @param yPos - the y position in the window
     @param xSize - the width of the button
     @param ySize - the height of the button
-    @param function - the function to be invoked when this button is pressed
+    @param command - the function to be invoked when this button is pressed
     """
-    def addButton(self, text, xPos, yPos, xSize, ySize, function):
-        button = Button(self.gui, width=xSize, height=ySize, command=function, text=text)
+    def addButton(self, text, xPos, yPos, xSize, ySize, command):
+        button = Button(self.gui, command=command, text=text)
         button.pack()
-        button.place(x=xPos, y=yPos)
+        button.place(x=xPos, y=yPos, width=xSize, height=ySize)
+        
+    """
+    adds a textBox to the gui, wich saves its content to a predefined variable
+    @param variable - the variable to be used and displayed
+    @param xPos - the x position in the window
+    @param yPos - the y position in the window
+    @param xSize - the width of the textBox
+    @param ySize - the height of the textBox
+    @param command - the function to be invoked when Enter is pressed
+    """
+    def addTextBox(self, variable, xPos, yPos, xSize, ySize, command):
+        textBox = Entry(self.gui, textvariable=variable)
+        textBox.pack()
+        textBox.bind('<Return>', command)
+        textBox.place(x=xPos, y=yPos, width=xSize, height=ySize)
         
     def test(self):
         print 1
+        
+    def test2(self, helpervar):
+        print 2
