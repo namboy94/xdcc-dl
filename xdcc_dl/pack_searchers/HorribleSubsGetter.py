333cc7baf8fa6d22e0bcb25dc8fca1fdf8eea33e
Hermann Krumrey <hermann@krumreyh.com>
2016-10-16 00:39:12 +0200

    Defined the PackSearcher class that unifies all pack searchers.

--- xdcc_dl/pack_searchers/HorribleSubsGetter.py
+++ xdcc_dl/pack_searchers/HorribleSubsGetter.py
@@ -1,26 +1,27 @@
 """
 LICENSE:
-Copyright 2015,2016 Hermann Krumrey
+Copyright 2016 Hermann Krumrey
 
-This file is part of toktokkie.
+This file is part of xdcc_dl.
 
-    toktokkie is a program that allows convenient managing of various
-    local media collections, mostly focused on video.
+    xdcc_dl is a program that allows downloading files via hte XDCC
+    protocol via file serving bots on IRC networks.
 
-    toktokkie is free software: you can redistribute it and/or modify
+    xdcc_dl is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    toktokkie is distributed in the hope that it will be useful,
+    xdcc_dl is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with toktokkie.  If not, see <http://www.gnu.org/licenses/>.
+    along with xdcc_dl.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE
 """
+
 # imports
 from typing import List
 
