e87c265671c948c6684afa0e9c93ffdfbab90c36
Hermann Krumrey <hermann@krumreyh.com>
2016-10-28 13:11:53 +0200

    Fixed documentation generating

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,14 +1,12 @@
-import sys
+#!/usr/bin/env python3
+
 import os
+import sys
 sys.path.insert(0, os.path.abspath("../.."))
 from xdcc_dl.metadata import General
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
 ]
 
 
@@ -16,40 +14,40 @@ templates_path = ['.templates']
 source_suffix = '.rst'
 master_doc = 'index'
 
-project = 'xdcc_dl'
+# noinspection PyShadowingBuiltins
 copyright = '2016, Hermann Krumrey'
 author = 'Hermann Krumrey'
+project = 'xdcc-downloader'
 
 version = General.version_number
 release = General.version_number
-
 language = None
+
+
 exclude_patterns = []
 pygments_style = 'sphinx'
-
-todo_include_todos = True
+todo_include_todos = False
 
 html_theme = 'alabaster'
 html_static_path = ['.static']
-htmlhelp_basename = 'xdcc_dldoc'
+htmlhelp_basename = 'xdcc-downloader-doc'
 
-# -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
 }
 latex_documents = [
-    (master_doc, 'xdcc_dl.tex', 'xdcc_dl Documentation',
+    (master_doc, 'xdcc-downloader.tex', 'xdcc-downloader Documentation',
      'Hermann Krumrey', 'manual'),
 ]
 
 man_pages = [
-    (master_doc, 'xdcc_dl', 'xdcc_dl Documentation',
+    (master_doc, 'xdcc-downloader', 'xdcc-downloader Documentation',
      [author], 1)
 ]
 
 texinfo_documents = [
-    (master_doc, 'xdcc_dl', 'xdcc_dl Documentation',
-     author, 'xdcc_dl', 'One line description of project.',
+    (master_doc, 'xdcc-downloader', 'xdcc-downloader Documentation',
+     author, 'xdcc-downloader', 'An XDCC Downloader bot',
      'Miscellaneous'),
 ]
 
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to xdcc_dl's documentation!
-===================================
+Welcome to xdcc-downloader's documentation!
+===========================================
 
 Contents:
 
