4f991e343a3589b31e5eb9c59ca55f3b14ad3f9f
Hermann Krumrey <hermann@krumreyh.com>
2016-11-10 01:00:49 +0100

    Implemented horriblesubs pack searcher

--- xdcc_dl/metadata.py
+++ xdcc_dl/metadata.py
@@ -127,7 +127,7 @@ class PypiVariables:  # pragma: no cover
     The list trove classifiers applicable to this project
     """
 
-    install_requires = ["raven", "irc", "bs4", "requests"]
+    install_requires = ["raven", "irc", "bs4", "requests", "cfscrape"]
     """
     Python Packaging Index dependencies
     """
