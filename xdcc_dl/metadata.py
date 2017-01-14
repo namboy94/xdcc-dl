0528b12e0de4cd042a77da411f56ded2f25cc68e
Hermann Krumrey <hermann@krumreyh.com>
2017-01-14 13:44:01 +0100

    Version bump to 1.1.1

--- xdcc_dl/metadata.py
+++ xdcc_dl/metadata.py
@@ -74,7 +74,7 @@ class General:  # pragma: no cover
     A short description of the project
     """
 
-    version_number = "1.1.0"
+    version_number = "1.1.1"
     """
     The current version of the program.
     """
